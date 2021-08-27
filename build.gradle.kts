a01f9ec2269f2cbbcc7def14e1e9151f4003ca8f
Jeff <zhsu@amwares.com>
2021-08-27 16:54:29 +0800

    设置项目的构建、依赖等等。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,41 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+	kotlin("jvm") version "1.4.32"
+	id("com.github.johnrengelman.shadow") version "4.0.4"
 }
 
 group = "cn.jeff.app"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenLocal()
+	mavenCentral()
+	maven("https://maven.aliyun.com/repository/public")
+	maven("https://maven.aliyun.com/nexus/content/groups/public/")
 }
 
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
 
+	implementation("no.tornado:tornadofx:1.7.17")
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+	// https://mvnrepository.com/artifact/com.google.code.gson/gson
+	implementation(
+		"com.google.code.gson",
+		name = "gson",
+		version = "2.8.6"
+	)
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.shadowJar {
+	manifest {
+		attributes["Main-Class"] = "cn.jeff.prj04.AppMain"
+	}
 }
