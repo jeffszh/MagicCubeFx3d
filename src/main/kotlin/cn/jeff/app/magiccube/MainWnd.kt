8a2ef9cecc11222b4254d5cbead72963cdb1a897
Jeff <zhsu@amwares.com>
2021-09-07 15:43:07 +0800

    根据官方例子（翻译成Kotlin），终于有东西看了。

--- src/main/kotlin/cn/jeff/app/magiccube/MainWnd.kt
+++ src/main/kotlin/cn/jeff/app/magiccube/MainWnd.kt
@@ -2,7 +2,15 @@ package cn.jeff.app.magiccube
 
 import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
+import javafx.scene.Group
+import javafx.scene.PerspectiveCamera
+import javafx.scene.SubScene
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.Color
+import javafx.scene.paint.PhongMaterial
+import javafx.scene.shape.Box
+import javafx.scene.transform.Rotate
+import javafx.scene.transform.Translate
 import tornadofx.*
 
 class MainWnd : View("3D魔方") {
@@ -21,6 +29,43 @@ class MainWnd : View("3D魔方") {
 		)
 		j = loader.getController()
 		j.k = this
+
+//		j.myBox.material = PhongMaterial(Color.RED)
+//		val camera = PerspectiveCamera().apply {
+//			transforms.addAll(
+//				Rotate(-30.0, Rotate.Y_AXIS),
+//				Rotate(-20.0, Rotate.X_AXIS),
+//				Translate(0.0, 0.0, -15.0)
+//			)
+//		}
+//		j.mainGroup.add(camera)
+//		j.mainGroup.add(SubScene(j.mainGroup, 300.0, 300.0).apply {
+//			fill = Color.ALICEBLUE
+//			this.camera = camera
+//		})
+
+		val cam1 = PerspectiveCamera(true).apply {
+			transforms.addAll(
+				Rotate(-30.0, Rotate.Y_AXIS),
+				Rotate(-20.0, Rotate.X_AXIS),
+				Translate(-0.0, 0.0, -15.0)
+			)
+		}
+		val g1 = Group().apply {
+			// add(cam1)	// 这句有或者没有都一样行，例子中是有的，不明所以。
+			add(Box(4.0, 5.0, 6.0).apply {
+				material = PhongMaterial(Color.RED)
+			})
+		}
+
+		root.center {
+			group {
+				add(SubScene(g1, 300.0, 300.0).apply {
+					fill = Color.AQUA
+					camera = cam1
+				})
+			}
+		}
 	}
 
 	fun btnClick(actionEvent: ActionEvent) {
